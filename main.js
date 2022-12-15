0cd3d8a69e254f7c92f47c19def5be13979905fb
Zgerskih <114507567+Zgerskih@users.noreply.github.com>
2022-12-15 19:00:44 +0300

    вывод списка

--- main.js
+++ main.js
@@ -1,52 +1,167 @@
-const domBtnPlus = document.getElementById('btnAddWorkItem');
-const domBtnClose = document.getElementById('btnCloseWorkItemPopup');
-const popup = document.getElementById('popup');
-const domInputQty = document.getElementById('inputWorkItemQty');
-const domInputCost = document.getElementById('inputWorkItemCost');
-const domItemTotal = document.getElementById('workItemTotalContainer');
-const domBtnCreateWorkItem = document.getElementById('btnCreateWorkItem');
-const domLiItem = document.getElementById('liItem');
+// button
+const btnOpenContainer = document.getElementById("btnAddItem");
+const btnCloceContainer = document.getElementById("btnCloceContainer");
+const btnCreateItem = document.getElementById("btnCreateItem");
 
-domBtnPlus.addEventListener('click', onBtnOpenAddWorkItem);
-domBtnClose.addEventListener('click', onBtnCloseAddWorkItem);
-domInputQty.addEventListener('keyup', totalItem);
-domInputCost.addEventListener('keyup', totalItem);
-domBtnCreateWorkItem.addEventListener("click", onCreateWorkItem)
+btnCreateItem.addEventListener("click", createItemSheet);
+btnCloceContainer.addEventListener("click", clocePopupContainer);
+btnOpenContainer.addEventListener("click", () => openPopupContainer());
 
-function totalItem() {
-  const qty = domInputQty.value;
-  const cost = domInputCost.value;
-  let total = qty * cost;
-  domItemTotal.innerHTML = total;
-  console.log(total, qty, cost);
-  return total;
+//  input
+const popupContainer = document.getElementById("popupContainer");
+const inpDocumentNumber = document.getElementById("documentNumber");
+const inpQtyElements = document.getElementById("inputQtyElements");
+const inputCostElements = document.getElementById("inputCostElements");
+const inputTotalElements = document.getElementById("inputTotalElements");
+const tableItems = document.getElementById("tableItems");
+const inpItemTitle = document.getElementById("inpItemTitle");
+const inpDescription = document.getElementById("inpDescription");
+const subtotalResult = document.getElementById("subtotalResult");
+const discountResult = document.getElementById("discountResult");
+const totalResult = document.getElementById("totalResult");
+const popupBackgroundBlocker = document.getElementById("popupBackgroundBlocker");
+
+inpDocumentNumber.addEventListener("keyup", () => inpDocumentNumberVerification());
+
+inpQtyElements.addEventListener("keyup", (event) => {
+  console.log("> inpQtyElements:", event.currentTarget.value);
+  currentWorkItem.qty = parseInt(event.currentTarget.value);
+  inputTotalElements.innerHTML = currentWorkItem.total;
+  checkCanCreate();
+});
+inpItemTitle.addEventListener("keyup", (event) => {
+  console.log("> inpItemTitle:", event.currentTarget.value);
+  currentWorkItem.title = event.currentTarget.value;
+  checkCanCreate();
+});
+inputCostElements.addEventListener("keyup", (event) => {
+  console.log("> inputCostElements:", event.currentTarget.value);
+  currentWorkItem.cost = parseInt(event.currentTarget.value);
+  inputTotalElements.innerHTML = currentWorkItem.total;
+  checkCanCreate();
+});
+
+tableItems.addEventListener("click", (e) => {
+  const target = e.target.tableItems;
+  // console.log("click -> ", target, target.dataset.todoid);
+  if (target.dataset.todoid) {
+    openPopupContainer(target.dataset.todoid);
+  }
+});
+console.log(tableItems);
+
+const checkCanCreate = () => {
+  const result = !(currentWorkItem.title.length > 0 && currentWorkItem.total > 0);
+  console.log("> checkCanCreate:", result);
+  btnCreateItem.disabled = result;
+};
+
+class InvoiceVO {
+  constructor() {
+    this.id = "";
+    this.items = [];
+    this.discount = 0;
+    this.iban = "";
+  }
 }
 
-function onBtnOpenAddWorkItem() {
-  popup.style.display = 'block';
+class WorkItemVO {
+  constructor(title = "", description = "", qty, cost) {
+    this.id = null;
+    this.title = title;
+    this.description = description;
+    this.qty = qty;
+    this.cost = cost;
+  }
+  get total() {
+    return (this.cost) * (this.qty);
+  }
 }
 
-function onBtnCloseAddWorkItem() {
-  popup.style.display = 'none';
+const invoiceVO = JSON.parse(localStorage.getItem("invoice")) || new InvoiceVO();
+let currentWorkItem = null;
+
+displayMessages();
+
+function openPopupContainer(index) {
+  console.log(index);
+  currentWorkItem = index ? Object.create({}, invoiceVO.items[parseInt(index)]) : new WorkItemVO();
+  console.log("> openPopupContainer -> currentWorkItem", currentWorkItem);
+  btnCreateItem.disabled = true;
+  popupContainer.style.display = "block";
+  inpQtyElements.value = currentWorkItem.qty;
 }
-// //
-// function onCreateWorkItem(event) {
-//   console.log('> domBtnCreateWorkItem -> handle(click)', event)
-// }
 
-//проверка на disabled
-// function onCreateWorkItemCheck () {
-//   let $texts = $('#inputWorkItemQty, #inputWorkItemCost');
-//   let $button = $('#btnCreateWorkItem');
-//   $texts.on("change", function() {
-//     let hasEmpty = $texts.filter(function() { return !$(this).val(); }).length > 0;
-//     $button.prop('disabled', hasEmpty);
-//   }).trigger('change');
-// };
+function clocePopupContainer() {
+  popupContainer.style.display = "none";
+}
 
-// function createWorkItem(){
-// document.createElement("li"){
-//   if (totalItem != 0 );
-//   return domLiItem
-//   }
-// }
+function inpDocumentNumberVerification() {
+  const invoiceDocumentNumber = inpDocumentNumber.value;
+  const checkingForNumber = invoiceDocumentNumber && !Number.isNaN(invoiceDocumentNumber);
+  if (checkingForNumber) {
+    return true;
+  } else {
+    alert("Enter the number");
+  }
+}
+
+function sumOfItemsQtyAndCost() {
+  const canCalculateTotal = currentWorkItem.qty && currentWorkItem.cost;
+  currentWorkItem.total = canCalculateTotal ? currentWorkItem.qty * currentWorkItem.cost : 0;
+  inputTotalElements.innerHTML = currentWorkItem.total;
+}
+function createItemSheet() {
+  if (currentWorkItem.id == null) {
+    currentWorkItem.id = Date.now();
+    invoiceVO.items.push(currentWorkItem);
+  } else {
+    const index = invoiceVO.items.findIndex((vo) => vo.id === currentWorkItem.id);
+    invoiceVO.items.splice(index, 1, currentWorkItem);
+  }
+
+  currentWorkItem = null;
+  displayMessages();
+  saveInvoice();
+}
+
+function saveInvoice() {
+  localStorage.setItem("invoice", JSON.stringify(invoiceVO));
+}
+
+function displayMessages() {
+  let listItems = "";
+  invoiceVO.items.forEach((workItemVO, index) => {
+    listItems += `
+      <tr data-todoid="${workItemVO.id}"
+    class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100"
+    for="item_${index}"
+      >
+    <td data-todoid="${index}"
+    class="pointer-events-none px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-800"
+    >
+    ${workItemVO.title} <span class="text-gray-500"><br>${workItemVO.description}</span>
+    </td>
+    <td
+    class="pointer-events-none text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+    >
+    ${workItemVO.qty}
+    </td>
+    <td
+    class="pointer-events-none text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+    >
+    ${workItemVO.cost}
+    </td>
+    <td
+    class="pointer-events-none text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+    >
+    $${workItemVO.total}
+    </td>
+    </tr>
+      `;
+    subtotalResult.innerHTML = invoiceVO.items.reduce(function (prev, curr) {
+      return prev + curr.total;
+    }, 0);
+  });
+  tableItems.innerHTML = listItems;
+}
